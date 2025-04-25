0f09d9c0fffbefbf02409e6e3887e1da97277c88
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2025-04-25 10:57:38 +0300

    update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,16 +3,16 @@ import "./Header.scss";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 export default function Header() {
-  return (
-    <header className="header">
-      <img src="/logo.svg" />
-      <div className="header__right">
-        <div className="header__user-data">
-          <Avatar />
-          <span className="header__username">@USER</span>
-        </div>
-        <Button variant="outlined">ВЫХОД</Button>
-      </div>
-    </header>
-  );
+    return (
+        <header className="header">
+            <img width="130px" src="/logo.png" />
+            <div className="header__right">
+                <div className="header__user-data">
+                    <Avatar />
+                    <span className="header__username">@USER</span>
+                </div>
+                <Button variant="outlined">ВЫХОД</Button>
+            </div>
+        </header>
+    );
 }
